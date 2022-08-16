77b54c06a86e7132ce8c05a330570ba75b907f71
mpvasco <paulavascoo@gmail.com>
2022-08-16 14:25:47 -0300

    forms login updated

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormCreateAccount(FlaskForm):
@@ -13,5 +13,6 @@ class FormCreateAccount(FlaskForm):
 class FormLogin(FlaskForm):
   email = StringField('E-mail', validators=[DataRequired(), Email()])
   password = PasswordField('Password', validators=[DataRequired(), Length(6,20)])
+  remember_me = BooleanField('Remmember access data')
   btn_submit_login = SubmitField('Login')
 
